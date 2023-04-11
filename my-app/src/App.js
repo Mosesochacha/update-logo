5791b96ff3d03b6a8319710156c502627d60c8b0
mosesochacha <ochachamoses443@@gmail.com>
2023-04-11 11:00:40 +0300

    Changed Logo

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg' className="App-logo" alt="logo" style={{width: "10em"}}/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
